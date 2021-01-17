b3a19f6147ee034c79c6df722e25d67150f7c2c3
Klaus Curde <kcurde@gmail.com>
2021-01-17 00:19:22 -0500

    added changes for balena desktop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,15 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch draculabar
-echo "---" | tee -a /tmp/draculabar.log 
-polybar draculabar >>/tmp/draculabar.log 2>&1 &
+#echo "---" | tee -a /tmp/draculabar.log 
+#polybar draculabar >>/tmp/draculabar.log 2>&1 &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar draculabar >>/tmp/draculabar.log 2>&1 &  
+  done
+else
+  polybar draculabar >>/tmp/draculabar.log 2>&1 &
+fi
 
 echo "Bars launched..."
