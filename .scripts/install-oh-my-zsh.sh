a8c372d0efc76bd2acf35ed47113baf07049d2ef
Klaire Curde <kcurde@gmail.com>
2024-06-21 12:56:22 -0400

    added auto installing zvm zsh plugin

--- .scripts/install-oh-my-zsh.sh
+++ .scripts/install-oh-my-zsh.sh
@@ -4,3 +4,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install zvm
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
